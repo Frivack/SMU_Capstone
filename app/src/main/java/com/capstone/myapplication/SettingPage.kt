abd9fd86f2a12622ac21c1fb48db3f137655aa70
siseo123 <tpdlf9801@gmail.com>
2024-11-04 15:02:28 +0900

    Android 틀

---
+++ app/src/main/java/com/capstone/myapplication/SettingPage.kt
@@ -0,0 +1,11 @@
+package com.capstone.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class settingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settingpage)
+    }
+}
