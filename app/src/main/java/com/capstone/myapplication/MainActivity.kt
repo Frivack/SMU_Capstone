6538d85fafa1cbcd5b8b64becfa80aa021cd24ba
Frivack <lmjx98@gmail.com>
2024-11-30 00:59:56 +0900

    모든 엑티비티 제작

--- app/src/main/java/com/capstone/myapplication/MainActivity.kt
+++ app/src/main/java/com/capstone/myapplication/MainActivity.kt
@@ -1,47 +1,23 @@
 package com.capstone.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.capstone.myapplication.ui.theme.EngUniFriendTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            EngUniFriendTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_start)
+
+        // 버튼이나 뷰 찾기
+        val page = findViewById<ImageView>(R.id.start_main_page)
+
+        // 클릭 리스너 설정
+        page.setOnClickListener {
+            // Intent를 사용해 NextActivity로 이동
+            val intent = Intent(this, MainPage::class.java)
+            startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EngUniFriendTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/capstone/myapplication/MainPage.kt
+++ app/src/main/java/com/capstone/myapplication/MainPage.kt
@@ -1,4 +1,11 @@
 package com.capstone.myapplication
 
-class MainPage {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainpage)
+    }
 }
